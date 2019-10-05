43df7f8d77676f46c7d4f75b3f408870c676298f
wons2 <53519832+wons2@users.noreply.github.com>
2019-10-05 19:44:11 +0900

    feat: AngleManager, STTManager 수정 및 추가

--- app/src/main/java/com/project/vedere/controller/SensorTestActivity.java
+++ app/src/main/java/com/project/vedere/controller/SensorTestActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -12,34 +13,30 @@ import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.view.Menu;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.project.vedere.R;
 import com.project.vedere.managers.AngleManager;
 import com.project.vedere.managers.STTManager;
 import com.skt.Tmap.TMapPoint;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 
 public class SensorTestActivity extends AppCompatActivity implements SensorEventListener{
 
-    private Vibrator vibrator;
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
     private Sensor mMagneticField;
-    private float mPitch, mRoll;
     private AngleManager angleManager;
-    float[] mGravity;
-    float[] mGeomagnetic;
-
-    // STT
-    Intent intent;
-    SpeechRecognizer mRecognizer;
-    Button sttBtn;
-    TextView textView;
-    final int PERMISSION = 1;
     private STTManager sttManager;
 
     @Override
@@ -52,24 +49,8 @@ public class SensorTestActivity extends AppCompatActivity implements SensorEvent
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
-        if( Build.VERSION.SDK_INT>=23 ){    //퍼미션체크
-            ActivityCompat.requestPermissions(this,new String[]{
-                    Manifest.permission.INTERNET,Manifest.permission.RECORD_AUDIO}, PERMISSION);
-        }
-        textView = (TextView)findViewById(R.id.sttResult);
-        sttBtn = (Button)findViewById(R.id.sttStart);
+        sttManager = new STTManager(this );
 
-        // 사용자에게 음성을 요구하고 음성 인식기를 통해 전송하는 활동 시작
-        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        // 음성 인식을 위한 음성 인식기의 의도에 사용되는 여분의 키
-        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName());
-        // 음성을 번역할 언어를 설정
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");
-        sttBtn.setOnClickListener(v -> {
-                mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-                mRecognizer.setRecognitionListener(sttManager.getListener());
-                mRecognizer.startListening(intent);
-        });
     }
 
     @Override
@@ -79,6 +60,7 @@ public class SensorTestActivity extends AppCompatActivity implements SensorEvent
                 mAccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener((SensorEventListener)this,
                 mMagneticField,SensorManager.SENSOR_DELAY_NORMAL);
+        sttManager.callSTTManager();
     }
 
     @Override
@@ -89,26 +71,7 @@ public class SensorTestActivity extends AppCompatActivity implements SensorEvent
 
     @Override
     public void onSensorChanged(SensorEvent event){
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
-            mGravity = event.values;
-        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
-            mGeomagnetic = event.values;
-        if (mGravity != null && mGeomagnetic != null) {
-            float[] R = new float[9];
-            float[] I = new float[9];
-            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
-            if (success) {
-                float[] orientation = new float[3];
-                SensorManager.getOrientation(R, orientation);
-                float azimuth = (float) Math.toDegrees(orientation[0]);
-
-                TMapPoint startPoint = new TMapPoint(37569758,126977022);
-                TMapPoint arrivePoint = new TMapPoint(37570594,126997589);
-                angleManager.setStartDirection(azimuth,startPoint,arrivePoint);
-                mPitch = (float) Math.toDegrees(orientation[1]);
-                mRoll = (float) Math.toDegrees(orientation[2]);
-            }
-        }
+        angleManager.callSetStartDirection(event);
     }
 
     @Override
--- app/src/main/java/com/project/vedere/managers/AngleManager.java
+++ app/src/main/java/com/project/vedere/managers/AngleManager.java
@@ -2,38 +2,59 @@ package com.project.vedere.managers;
 
 import android.app.Activity;
 import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.os.Vibrator;
 import android.util.Log;
 
-import com.project.vedere.model.DirectionInfo;
 import com.skt.Tmap.TMapPoint;
 
-import java.util.Queue;
-
+import static android.content.Context.SENSOR_SERVICE;
 
 public class AngleManager{
 
-    private Activity activity;
     private Vibrator vibrator;
-    private DirectionInfo dirInfo;
+    private float[] mGravity;
+    private float[] mGeomagnetic;
 
     public AngleManager(Activity activity) {
-        this.activity = activity;
         vibrator = (Vibrator)activity.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
-    public void setStartDirection(float azimuth, TMapPoint startPoint, TMapPoint arrivePoint) {
-        Log.d("azimuth",Float.toString(azimuth));
-        Log.d("angle",Float.toString(CalculateBearingAngle(startPoint,arrivePoint)));
-        if(Math.abs(azimuth-CalculateBearingAngle(startPoint,arrivePoint))<=5) {    // 나침반 방향과 가야할 방향이 같으면
-            vibrator.vibrate(1000 ); // 1초간 진동
-            Log.d("directionright","올바른방향");
-
+    public void callSetStartDirection(SensorEvent event){
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            mGravity = event.values;
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            mGeomagnetic = event.values;
+        if (mGravity != null && mGeomagnetic != null) {
+            float[] R = new float[9];
+            float[] I = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+            if (success) {
+                float[] orientation = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                float azimuth = (float) Math.toDegrees(orientation[0]);
+                if( azimuth<0.0f )
+                    azimuth += 360.f;
+                TMapPoint startPoint = new TMapPoint(37569758,126977022);
+                TMapPoint arrivePoint = new TMapPoint(37570594,126997589);
+                setStartDirection(azimuth,startPoint,arrivePoint);
+            }
         }
-        else if(Math.abs(azimuth-CalculateBearingAngle(startPoint,arrivePoint))>5) {   // 다르면
+    }
+
+
+    public void setStartDirection(float azimuth, TMapPoint startPoint, TMapPoint arrivePoint) {
+        if(Math.abs(azimuth-CalculateBearingAngle(startPoint,arrivePoint))<=10.0f) {    // 나침반 방향과 가야할 방향이 같으면
+            vibrator.vibrate(1000 ); // 1초간 진동
+            //Log.d("direction","올바른방향");
+        }
+        else{   // 다르면
             vibrator.cancel();
-            Log.d("directionwrong","틀린방향");
+            //Log.d("direction","틀린방향");
         }
     }
 
@@ -47,144 +68,8 @@ public class AngleManager{
         destLoc.setLatitude(arrivePoint.getLatitude());
         destLoc.setLongitude(arrivePoint.getLongitude());
         float bearTo = userLoc.bearingTo(destLoc);
-        if( bearTo < 0 )
-            bearTo += 360;
+        if( bearTo < 0.0f )
+            bearTo += 360.0f;
         return bearTo;
     }
-
-    public void setAllTurnInfo(Queue<DirectionInfo> queue){
-        DirectionInfo dirInfo = queue.poll();
-        int turnInfo = dirInfo.getTurnInfo();
-        if( turnInfo==0 ){
-            double angle = calculateAngle(dirInfo.getStartPoint(),dirInfo.getArrivePoint(),dirInfo.getPriorStartPoint());
-            if( -15.0<angle && angle<15.0 )
-                dirInfo.setTurnInfo(11);    // 직진
-            else if( 15.0<=angle && angle<45.0 )
-                dirInfo.setTurnInfo(180);   //1시방향 우회전
-            else if( 45.0<=angle && angle<75.0 )
-                dirInfo.setTurnInfo(18);    // 2시방향 우회전
-            else if( 75.0<=angle && angle<105.0 )
-                dirInfo.setTurnInfo(13);   // 3시방향 우회전
-            else if( 105.0<=angle && angle<135.0 )
-                dirInfo.setTurnInfo(19);    // 4시방향 우회전
-            else if( 135.0<=angle && angle<165.0 )
-                dirInfo.setTurnInfo(20);    // 5시방향 우회전
-            else if( (165.0<=angle && angle<=180.0 ) || (-180<=angle && angle<=165) )
-                dirInfo.setTurnInfo(14);    // 유턴
-            else if( -45.0<=angle && angle<=-15 )
-                dirInfo.setTurnInfo(170);   // 11시 방향 좌회전
-            else if( -75.0<=angle && angle<-45.0 )
-                dirInfo.setTurnInfo(17);    // 10시 방향 죄화전
-            else if( -105.0<=angle && angle<-75.0 )
-                dirInfo.setTurnInfo(160);   // 9시 방향 좌회전
-            else if( -135.0<=angle && angle<-105.0 )
-                dirInfo.setTurnInfo(16);    // 8시 방향 좌회전
-            else if( -165.0<angle && angle<-135.0 )
-                dirInfo.setTurnInfo(15);    // 7시 방향 좌회전
-
-        }
-    }
-
-    public String setGotoDirection(DirectionInfo dirInfo) {
-        int turnInfo = dirInfo.getTurnInfo();
-
-        String text = "";
-        switch (turnInfo){
-            case 11:
-                text = "다음 안내가 나올 때 까지 직진하세요.";
-                break;
-            case 12:
-                text = "좌회전 하세요";
-                break;
-            case 13:
-                text = "우회전 하세요";
-                break;
-            case 14:
-                text = "유턴 하세요";
-                break;
-            case 15:
-                text = "7시 방향 좌회전하세요.";
-                break;
-            case 16:
-                text = "8시 방향 좌회전하세요";
-                break;
-            case 160:
-                text = "9시 방향 좌회전하세요";
-                break;
-            case 17:
-                text = "10시 방향 좌회전하세요";
-                break;
-            case 170:
-                text = "11시 방향 좌회전하세요";
-                break;
-            case 180:
-                text = "1시방향 우회전하세요";
-                break;
-            case 18:
-                text = "2시 방향 우회전하세요";
-                break;
-            case 19:
-                text = "4시 방향 우회전하세요";
-                break;
-            case 20:
-                text = "5시 방향 우회전하세요";
-                break;
-            case 125:
-                text = "육교입니다";
-                break;
-            case 126:
-                text = "지하보도입니다";
-                break;
-            case 127:
-                text = "계단에 진입합니다";
-                break;
-            case 128:
-                text = "경사로에 진입합니다";
-                break;
-            case 129:
-                text = "계단과 경사로에 진입합니다.";
-                break;
-            case 201:
-                text = "목적지에 도착했습니다. 안내를 종료합니다.";
-                break;
-            case 211:
-                text = "횡단보도입니다.";
-                break;
-            case 212:
-                text = "좌측 횡단보도입니다.";
-                break;
-            case 213:
-                text = "우측 횡단보도입니다.";
-                break;
-            case 214:
-                text = "8시 방향 횡단보도입니다.";
-                break;
-            case 215:
-                text = "10시 방향 횡단보도입니다.";
-                break;
-            case 216:
-                text = "2시 방향 횡단보도입니다.";
-                break;
-            case 217:
-                text = "4시 방향 횡단보도입니다.";
-                break;
-            case 218:
-                text = "엘리베이터입니다.";
-                break;
-            case 233:
-                text = "직진 임시 입니다.";
-                break;
-        }
-        return text;
-    }
-
-    private double calculateAngle(TMapPoint startPoint,TMapPoint arrivePoint,TMapPoint priorPoint){
-        double angle = Math.atan2(arrivePoint.getLatitude()-startPoint.getLatitude(),arrivePoint.getLongitude()-startPoint.getLongitude())
-                        -Math.atan2(priorPoint.getLatitude()-startPoint.getLatitude(),priorPoint.getLongitude()-startPoint.getLongitude());
-        angle *= 360.0/(2.0*Math.PI);
-        // 0<=angle<=180이면 오른쪽으로 angle만큼 돌아야함
-        // -180<=angle<=0이면 왼쪽으로 angle만큼 돌아야함
-        return angle;
-    }
-
 }
